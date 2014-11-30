87b2d643defedb62c9939920e6fa94fdabd63914
Dan Nuffer <danielnuffer@gmail.com>
2014-11-29 21:06:58 -0700

    Save tasks marked as completed, delete tasks.

--- client/templates/tasks/task_item.js
+++ client/templates/tasks/task_item.js
@@ -1,3 +1,13 @@
 Template.taskItem.rendered = function() {
     $('.datetimepicker').datetimepicker();
 }
+
+Template.taskItem.events({
+  "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Meteor.call("setChecked", this._id, ! this.checked);
+  },
+  "click .delete": function () {
+    Meteor.call("deleteTask", this._id);
+  }
+});
